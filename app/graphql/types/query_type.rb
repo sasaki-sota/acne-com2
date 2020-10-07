d141787c4e569d069be9124886ee25776f730bb6
soutaschool <sasakisouta110@icloud.com>
2020-10-07 15:14:32 +0900

    Postモデルの追加

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,13 +1,17 @@
 module Types
   class QueryType < Types::BaseObject
-    # Add root-level fields here.
-    # They will be entry points for queries on your schema.
+    field :posts, [Types::PostType], null: false
 
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    def posts
+      Post.all
+    end
+
+    field :post, Types::PostType, null: false do
+      argument :id, Int, required: false
+    end
+
+    def post(id:)
+      Post.find(id)
     end
   end
 end
