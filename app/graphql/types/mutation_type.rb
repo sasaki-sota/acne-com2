b664feadbbeb725600185d3a3e88748c586b8578
soutaschool <sasakisouta110@icloud.com>
2020-10-08 23:43:51 +0900

    一通りの作成の仕方done

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,5 +1,6 @@
 module Types
   class MutationType < Types::BaseObject
+    field :create_comment, mutation: Mutations::CreateComment
     field :delete_post, mutation: Mutations::DeletePost
     field :create_post, mutation: Mutations::CreatePost
     field :update_post, mutation: Mutations::UpdatePost
