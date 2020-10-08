9a8b1ac247f7afaba097f94be415a52846802c88
soutaschool <sasakisouta110@icloud.com>
2020-10-08 23:22:46 +0900

    delete-post-mutationsの実装

--- app/graphql/types/post_type.rb
+++ app/graphql/types/post_type.rb
@@ -5,5 +5,7 @@ module Types
     field :description, String, null: false
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+    # アソシエーションのものは配列で括ることを忘れない
+    field :comments, [Types::CommentType], null: false
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_06_135808) do
+ActiveRecord::Schema.define(version: 2020_10_08_140322) do
 
-  create_table "posts", force: :cascade do |t|
-    t.string "title", null: false
-    t.text "description", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+  create_table 'comments', force: :cascade do |t|
+    t.string 'content'
+    t.integer 'post_id', null: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['post_id'], name: 'index_comments_on_post_id'
   end
 
+  create_table 'posts', force: :cascade do |t|
+    t.string 'title', null: false
+    t.text 'description', null: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+  end
+
+  add_foreign_key 'comments', 'posts'
 end
